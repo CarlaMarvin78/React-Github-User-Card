0a319e076c32e436168a4e2f38c4b91449b1cb05
Carla Marvin <carlamarvin78@gmail.com>
2019-08-06 22:37:15 -0400

    MVP complete

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Card from './Components/Card';
+import UserList from './Components/UserList';
 
 
 
@@ -8,31 +8,45 @@ class App extends React.Component{
   constructor () {
     super ();
     this.state={
-      user: {}
+      users: []
        }
     }
-    fetchUser = () => {
+    fetchFollowers=(url)=>{
+      fetch(url)
+      .then(response=>response.json())
+      .then(followers => {
+        followers.forEach(follower=>{
+          fetch(follower.url)
+          .then(response => response.json())
+          .then(user=> this.setState({users: [...this.state.users, user]}))
+        })
+      })
+    };
+    fetchUsers = () => {
       fetch('https://api.github.com/users/CarlaMarvin78')
         
       .then(response => {
         return response.json();
       })
-        .then(user => this.setState({user: user}))
+        .then(user => {
+          this.setState({users: [...this.state.users, user]});
+          this.fetchFollowers(user.followers_url);
+        })
         .catch(err => {
           console.log(err);
           
         });
     };
     componentDidMount() {
-      this.fetchUser();
+      this.fetchUsers();
     }
   render () {    
     return  (
     <div className="App">
       <header className="App">
-        Github Users
+        <h1>Github Users</h1>
       </header>
-      <Card user={this.state.user}/>
+      <UserList users={this.state.users}/>
     </div>
   );
     }
